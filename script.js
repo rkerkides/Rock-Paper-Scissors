20983f2f59ac7cacc5442e809af589b44238f7d0
Renos Kerkides <rkerkides@gmail.com>
2023-04-05 23:28:23 +0300

    Add function "game" & rename "play" to "playRound"

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Global variables
+let ties = 0;
+let wins = 0;
+let losses = 0;
+
 // Functions
 function getComputerChoice() {
     switch (Math.floor(Math.random() * 3) + 1) {
@@ -10,45 +15,67 @@ function getComputerChoice() {
     }
 }
 
-function play(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
     let player = playerSelection.toUpperCase();
     let computer = computerSelection.toUpperCase();
     if (player == "ROCK") {
         if (computer == "ROCK") {
+            ties++;
             return "It's a tie!";
         }
         if (computer == "PAPER") {
+            losses++;
             return "You Lose! Paper beats Rock";
         }
         if (computer == "SCISSORS") {
+            wins++;
             return "You Win! Rock beats Scissors";
         }
     }
     if (player == "PAPER") {
         if (computer == "PAPER") {
+            ties++;
             return "It's a tie!";
         }
         if (computer == "ROCK") {
+            wins++;
             return "You Win! Paper beats Rock";
         }
         if (computer == "SCISSORS") {
+            losses++;
             return "You Lose! Scissors beats Paper";
         }
     }
     if (player == "SCISSORS") {
         if (computer == "SCISSORS") {
+            ties++;
             return "It's a tie!";
         }
         if (computer == "ROCK") {
+            losses++;
             return "You Lose! Rock beats Scissors";
         }
         if (computer == "PAPER") {
+            wins++;
             return "You Win! Scissors beats Paper";
         }
     }
 }
 
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerChoice = prompt("Please type either 'Rock', 'Paper', or 'Scissors'!");
+        let compChoice = getComputerChoice();
+        console.log(playRound(playerChoice, compChoice));
+    }
+    if (wins > losses) {
+        console.log("You Win the game!")
+    } else if (losses > wins) {
+        console.log("You Lose the game!")
+    } else {
+        console.log("The game has resulted in an overall Tie!")
+    }
+}
+
 // main
-let compChoice = getComputerChoice();
-console.log(compChoice);
-console.log(play("roCK", compChoice))
+game();
