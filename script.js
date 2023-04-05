33d79b678b2716d08f645a5c2b622d834b4038b6
Renos Kerkides <rkerkides@gmail.com>
2023-04-05 23:13:26 +0300

    Add function "play" for main gameplay loop

--- script.js
+++ script.js
@@ -10,5 +10,45 @@ function getComputerChoice() {
     }
 }
 
+function play(playerSelection, computerSelection) {
+    let player = playerSelection.toUpperCase();
+    let computer = computerSelection.toUpperCase();
+    if (player == "ROCK") {
+        if (computer == "ROCK") {
+            return "It's a tie!";
+        }
+        if (computer == "PAPER") {
+            return "You Lose! Paper beats Rock";
+        }
+        if (computer == "SCISSORS") {
+            return "You Win! Rock beats Scissors";
+        }
+    }
+    if (player == "PAPER") {
+        if (computer == "PAPER") {
+            return "It's a tie!";
+        }
+        if (computer == "ROCK") {
+            return "You Win! Paper beats Rock";
+        }
+        if (computer == "SCISSORS") {
+            return "You Lose! Scissors beats Paper";
+        }
+    }
+    if (player == "SCISSORS") {
+        if (computer == "SCISSORS") {
+            return "It's a tie!";
+        }
+        if (computer == "ROCK") {
+            return "You Lose! Rock beats Scissors";
+        }
+        if (computer == "PAPER") {
+            return "You Win! Scissors beats Paper";
+        }
+    }
+}
+
 // main
-console.log(getComputerChoice());
+let compChoice = getComputerChoice();
+console.log(compChoice);
+console.log(play("roCK", compChoice))
