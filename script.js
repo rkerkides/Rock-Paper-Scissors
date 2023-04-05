3e6c0d7464772038e9dff5d543a9ec0192cd6423
Renos Kerkides <rkerkides@gmail.com>
2023-04-05 23:02:27 +0300

    Add function getComputerChoice

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 // Functions
-
+function getComputerChoice() {
+    switch (Math.floor(Math.random() * 3) + 1) {
+        case 1:
+            return "Rock";
+        case 2:
+            return "Paper";
+        case 3:
+            return "Scissors";
+    }
+}
 
 // main
+console.log(getComputerChoice());
